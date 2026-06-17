e54a78593b538751a4c5e56501ad0f5971edd43b
Baati Yosri <baatiyossri@gmail.com>
2026-06-17 09:46:30 +0200

    dleted test files and unecessary ones

--- test_entrepot.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('NOLOGIN', '1');
-require '../../main.inc.php';
-
-$res = $db->query("SHOW COLUMNS FROM llx_entrepot");
-while($obj = $db->fetch_object($res)) {
-    echo $obj->Field . "\n";
-}
--- test_url.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('NOLOGIN', '1');
-require '../../main.inc.php';
-echo dol_buildpath('/calcul_stock/admin/setup.php', 1) . "\n";
-echo dol_buildpath('/calcul_stock/setup.php', 1) . "\n";
